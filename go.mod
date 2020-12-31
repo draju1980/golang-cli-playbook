0634b2ac002a1747a7e58c0601d7b0d577626b0d
draju1980 <draju@outlook.com>
2020-12-31 10:55:28 +0000

    module1

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/draju1980/golang-cli-playbook
+module github.com/pluralsight-projects/golang-cli-playbook
 
 go 1.13
--- module1/module1_test.go
+++ module1/module1_test.go
@@ -13,7 +13,7 @@ import (
 func TestModule1CheckEnvGOOS(t *testing.T) {
 	found := OpenFileAndFindNthString("module1.txt", 0, "GOOS")
 
-	if !found {
+	if !found { 
 		t.Errorf("'go env' does not work as expected")
 	}
 }
